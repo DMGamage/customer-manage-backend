df5742e8768fd5046b2bfa3b674a3b09f55049ed
DMGamage <gamagemadhushan@gmail.com>
2023-01-07 19:24:47 +0530

    Finish Custom Exception Design Part

--- src/main/java/lk/ijse/dep9/controller/UserController.java
+++ src/main/java/lk/ijse/dep9/controller/UserController.java
@@ -1,13 +1,15 @@
 package lk.ijse.dep9.controller;
 
+import lk.ijse.dep9.exception.UserNotFoundException;
 import lk.ijse.dep9.model.User;
 import lk.ijse.dep9.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 
 public class UserController {
     @Autowired
@@ -18,6 +20,17 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return (List<User>) userRepository.findAll();
+
+    }
+    @GetMapping("user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
 
 }
---
+++ src/main/java/lk/ijse/dep9/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep9.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not found the user with id"+id);
+    }
+
+}
